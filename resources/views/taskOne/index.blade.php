1e058395e492fdb4117a3326e21a5a45e3169987
FahimSakib <fsakib991@gmail.com>
2022-08-08 22:02:15 +0600

    changes

--- resources/views/taskOne/index.blade.php
+++ resources/views/taskOne/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<a class="btn btn-primary" href="{{ route('task.one.save') }}" role="button">Save data from API</a>
+<a class="btn btn-primary" href="{{ route('task.one.save') }}" role="button">Generate Report</a>
 <a class="btn btn-primary" href="{{ route('task.one.report') }}" role="button">View Report</a>
 </div>
 @endsection
